f31d8f19e34fb521f2fc8c2c2a1b9ed96f2d07b2
szegedidani <szegedi.dani22@gmail.com>
2024-05-22 16:01:45 +0200

    Load pdf and autoscroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  pdfSrc: string = '';
+
+  public loadComplete() {
+    setTimeout(() => {
+      setInterval(() => {
+        const pdf = document.querySelector('.ng2-pdf-viewer-container');
+        if (!pdf) return;
+        pdf.scrollTop += 1;
+      }, 20)
+
+    }, 1000);
+  }
+
+  onFileSelected() {
+    let $img: any = document.querySelector('#file');
+
+    if (typeof (FileReader) !== 'undefined') {
+      let reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+
+      reader.readAsArrayBuffer($img.files[0]);
+    }
+  }
 }
